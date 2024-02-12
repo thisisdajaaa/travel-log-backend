9122e181a85d93681c2248f344782b3d38bba8f7
DAJA <33202282+DAJAKMPM@users.noreply.github.com>
2024-02-11 21:41:59 -0600

    feat: initial commit

---
+++ src/main/java/com/travellog/travellog/TravelLogApplication.java
@@ -0,0 +1,13 @@
+package com.travellog.travellog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelLogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelLogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/travellog/travellog/TravelLogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.travellog.travellog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelLogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
