62d6404880a0327479edd8196f93b7eb78cdee92
DAJA <33202282+DAJAKMPM@users.noreply.github.com>
2024-02-14 22:25:24 -0600

    feat: initial commit

--- src/main/java/com/travellog/travellog/configurations/SecurityConfiguration.java
+++ src/main/java/com/travellog/travellog/configurations/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package com.travellog.travellog.configurations;
 
 import com.travellog.travellog.helpers.JWTAuthenticationFilterHelper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
@@ -14,6 +15,9 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.web.cors.CorsConfiguration;
+
+import java.util.List;
 
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
@@ -21,6 +25,17 @@ import static org.springframework.security.config.http.SessionCreationPolicy.STA
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfiguration {
+    @Value("#{'${cors.allowed-origins}'.split(',')}")
+    private List<String> allowedOrigins;
+
+    @Value("#{'${cors.allowed-methods}'.split(',')}")
+    private List<String> allowedMethods;
+    @Value("#{'${cors.allowed-headers}'.split(',')}")
+    private List<String> allowedHeaders;
+
+    @Value("#{'${cors.exposed-headers}'.split(',')}")
+    private List<String> exposedHeaders;
+
     private static final String[] WHITE_LIST_URL = {"/api/v1/auth/**"};
     private final AuthenticationProvider authenticationProvider;
     private final JWTAuthenticationFilterHelper jwtAuthenticationFilterHelper;
@@ -37,7 +52,15 @@ public class SecurityConfiguration {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
-                .cors(Customizer.withDefaults())
+                .cors(cors -> cors.configurationSource(request -> {
+                    CorsConfiguration configuration = new CorsConfiguration();
+                    configuration.setAllowedOrigins(allowedOrigins);
+                    configuration.setAllowedMethods(allowedMethods);
+                    configuration.setAllowedHeaders(allowedHeaders);
+                    configuration.setExposedHeaders(exposedHeaders);
+                    configuration.addAllowedOrigin("http://localhost:3000");
+                    return configuration;
+                }))
                 .authorizeHttpRequests(req ->
                         req.requestMatchers(WHITE_LIST_URL)
                             .permitAll()
