1704729d16faa32d0d663101c29d62a599c4889c
Gaurav Neupane <ngaurav456@gmail.com>
2024-02-21 16:29:46 -0600

    feat(minio): minio initialized

---
+++ src/main/java/com/travellog/travellog/controllers/ImageController.java
@@ -0,0 +1,13 @@
+package com.travellog.travellog.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ImageController {
+    
+    @GetMapping 
+    public void test(){
+        System.out.println("hello");
+    }
+}
